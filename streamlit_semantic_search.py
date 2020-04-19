892a46ea40cd6a83c0d859b40a6e04f16433c818
renuka90 <renukagurung90@gmail.com>
2020-04-19 10:16:44 +0200

    updated code

--- streamlit_semantic_search.py
+++ streamlit_semantic_search.py
@@ -9,13 +9,7 @@ import streamlit as st
 from gensim.models import Word2Vec
 import re
 import pandas as pd
-#from wordcloud import WordCloud
-#import matplotlib.pyplot as plt
-#from sklearn.manifold import TSNE
-#import numpy as np
-#import matplotlib.cm as cm
-#import webbrowser
-#from IPython.display import HTML
+
 
 st.title('Semantic Search Engine')
 st.subheader('Find your relevant terms : similar or related')
@@ -23,55 +17,47 @@ st.subheader('Find your relevant terms : similar or related')
 model = Word2Vec.load('./data/article_data.model')
 
 pos_str = st.text_input('Enter keyword(s)')
-#neg_str = st.text_input('(Optional) Specify as unlike the following word(s)')
-
-pos_str = re.sub(' +',' ',  re.sub('\W', ' ', pos_str))
-#neg_str = re.sub(' +',' ',  re.sub('\W', ' ', neg_str))
+pos_str = pos_str.lower()
+# remove spaces both in the beginning and in the end of of string
+pos_str = re.sub("^\s+|\s+$", "", pos_str, flags=re.UNICODE)
 
 # any input that is NOT a-z, A-Z, 0-9,-,*
-#pos_str = re.sub('[^a-zA-Z0-9-_*.]', '', pos_str)
+pos_str = re.sub('[^a-zA-Z0-9-_*.]', ' ', pos_str)
+pos_str = re.sub(' +',' ',  re.sub('\W', ' ', pos_str))
+#lemmatizer = WordNetLemmatizer() 
 
-#pos_str = pos_str.lower()
+#pos_words = lemmatizer.lemmatize(pos_str)
+pos_words= pos_str.split(' ')
 
-pos_words = pos_str.split(' ')
-#neg_words = neg_str.split(' ')
-
-
+st.text('Your input')
+st.write('SIMILAR TO ', pos_str)
 
 if (len(pos_words[0]) > 0):
 
-    df = pd.DataFrame(model.wv.most_similar(positive = [pos_str], topn=10), columns = ['SIMILAR_word', 'similarity_score'])
+    df = pd.DataFrame(model.wv.most_similar(positive = pos_words, topn=10), columns = ['SIMILAR_word', 'similarity'])
     df1 = df[['SIMILAR_word']]
-    
-   #df2 = df1.style.hide_index()
     link_list = []
     for i in df['SIMILAR_word']:
-                     
+                  
         word = 'https://scholar.google.nl/scholar?hl=nl&as_sdt=0%2C5&q=' + i
-    
+ 
         link_list.append(word)
-        # rename column as SIMILAR for UI
+     # rename column as SIMILAR for UI
+    
     df1['SIMILAR'] = link_list
-    
-    #st.dataframe(df1)
-    
+     
+     #st.dataframe(df1)
+     
     df1 = df1[['SIMILAR']]
     def make_clickable(link):
-        # target _blank to open new window
-        # extract clickable text to display for your link
-        text = link.split('=')[3]
-        return f'<a target="_blank" href="{link}">{text}</a>'
-
-    # link is the column with hyperlinks
+         # target _blank to open new window
+         # extract clickable text to display for your link
+         text = link.split('=')[3]
+         return f'<a target="_blank" href="{link}">{text}</a>'
+     
+     # link is the column with hyperlinks
     df1['SIMILAR'] = df1['SIMILAR'].apply(make_clickable)
-    
-    html = (
-    df1.style
-    .set_properties(**{'border': '1px solid silver','font-size': '9pt', 'color':'red','font-family': 'Calibri'})
-   # .bar(subset=['col4', 'col5'], color='lightblue')
-    .render()
-)
+
     df1 = df1.to_html(escape=False)
     st.write(df1, unsafe_allow_html=True)
-        
         
