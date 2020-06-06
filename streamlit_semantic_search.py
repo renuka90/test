3ca33aff197b071e44912612412c268fdd5b5eb0
renuka90 <renukagurung90@gmail.com>
2020-06-06 12:57:28 +0200

    removed uploader

--- streamlit_semantic_search.py
+++ streamlit_semantic_search.py
@@ -96,65 +96,41 @@ st.subheader('Find the similar terms.')
 model = Word2Vec.load('./data/w2v_exclude_invalid_terms_1.model')
 
 #load exclud list items
-# df_exclude = pd.read_excel('./data/exclude_list.xlsx', sep=r'\s*,\s*',header=0, encoding='ascii')  
-uploaded_file = st.file_uploader("./data/exclude_list.xlsx", type="xlsx")
+df_exclude = pd.read_excel('./data/exclude_list.xlsx', sep=r'\s*,\s*',header=0, encoding='ascii')  
+df_list = df_exclude['word_list'].values.tolist()
 
-if uploaded_file:
-    df_exclude = pd.read_excel(uploaded_file) 
-    df_list = df_exclude['word_list'].values.tolist()
+#get model vocab index
+model_vocab = model.wv.index2word
 
-    #get model vocab index
-    model_vocab = model.wv.index2word
+# exclude the terms that appears in the exclude list.
+vocab_exclude_invalid_item = [i for i in model_vocab if i not in df_list]
+w2v_developed = restrict_w2v(model, vocab_exclude_invalid_item)
 
-    # exclude the terms that appears in the exclude list.
-    vocab_exclude_invalid_item = [i for i in model_vocab if i not in df_list]
-    w2v_developed = restrict_w2v(model, vocab_exclude_invalid_item)
+# get model vocab
+get_model = w2v_developed.wv.vocab
+#define list to store model
+list_model = []
+for i in get_model:
+    list_model.append(i)
 
-    # get model vocab
-    get_model = w2v_developed.wv.vocab
-    #define list to store model
-    list_model = []
-    for i in get_model:
-        list_model.append(i)
+# function to get the closest word with highest similarity score
+def calcJaroDistance(word, numWords):
 
-    # function to get the closest word with highest similarity score
-    def calcJaroDistance(word, numWords):
+    temp = (word.split())
 
-        temp = (word.split())
-
-        if len(temp) > 1:
-                  
-            word_list =[]
-            word_len =[]
+    if len(temp) > 1:
+              
+        word_list =[]
+        word_len =[]
+        
+        for i in temp:
             
-            for i in temp:
-                
-                dictWordDist = []
-                word_sim = []
-                
-                for line in list_model:  
-                    
-                    wordDistance = jaro_similarity(i, line)
-                    word_sim.append(float(wordDistance))
-                    dictWordDist.append(line)
-
-                d = {'word':dictWordDist,'word_sim':word_sim}
-
-                # Convert list to pandas
-                df = pd.DataFrame(d)
-                df = df.sort_values(by='word_sim', ascending=False)
-                df = df.loc[(df['word_sim']<=1)]
-                #print(df.iloc[:numWords])
-                word_list.append(df['word'].iloc[0])
-                word_len.append(df.iloc[:numWords])
-            return word_list
-        else: 
-         
             dictWordDist = []
             word_sim = []
-
-            for line in list_model:          
-                wordDistance = jaro_similarity(word, line)
+            
+            for line in list_model:  
+                
+                wordDistance = jaro_similarity(i, line)
                 word_sim.append(float(wordDistance))
                 dictWordDist.append(line)
 
@@ -163,122 +139,142 @@ if uploaded_file:
             # Convert list to pandas
             df = pd.DataFrame(d)
             df = df.sort_values(by='word_sim', ascending=False)
-
-            return df.iloc[:numWords]
-
-    #get input word
-    pos_str = st.text_input('Enter keyword(s)')
-    pos_str = pos_str.lower()
-
-    # convert the model into vectors
-    word_vectors = model.wv
-        
-    # Tokenize: Split the sentence into words
-    word_list = nltk.word_tokenize(pos_str)
-
-    lemmatizer = WordNetLemmatizer()
-    # Lemmatize list of words and join
-    pos_str = ' '.join([lemmatizer.lemmatize(w) for w in word_list])
-
-    #textblob for spelling correction
-    check_spel = pos_str
-    pos_str = TextBlob(check_spel)  
+            df = df.loc[(df['word_sim']<=1)]
+            #print(df.iloc[:numWords])
+            word_list.append(df['word'].iloc[0])
+            word_len.append(df.iloc[:numWords])
+        return word_list
+    else: 
      
-    try:
-        if check_spel in word_vectors.vocab:
-            
-            pos_str = str(pos_str)
-            # remove spaces both in the beginning and in the end of of string
-            pos_str = re.sub("^\s+|\s+$", "", pos_str, flags=re.UNICODE)
-            
-            # any input that is NOT a-z, A-Z, 0-9,-,*
-            pos_str = re.sub('[^a-zA-Z0-9-_*.]', ' ', pos_str)
-            pos_str = re.sub(' +',' ',  re.sub('\W', ' ', pos_str))
-            
-            pos_words= pos_str.split(' ')
-            
-            
-            if (len(pos_words[0]) > 0):
-            
-                st.write('SIMILAR TO ', pos_str)
-                df = pd.DataFrame(w2v_developed.wv.most_similar(positive = pos_words, topn=10), columns = ['SIMILAR_word', 'similarity'])
-                df1 = df[['SIMILAR_word']]
-                link_list = []
-                for i in df['SIMILAR_word']:
-                              
-                    word = 'https://scholar.google.nl/scholar?hl=nl&as_sdt=0%2C5&q=' + i
-             
-                    link_list.append(word)
-                 # rename column as SIMILAR for UI
-                
-                df1['SIMILAR'] = link_list
-                              
-                df1 = df1[['SIMILAR']]
-                def make_clickable(link):
-                     # target _blank to open new window
-                     # extract clickable text to display for your link
-                     text = link.split('=')[3]
-                     return f'<a target="_blank" href="{link+" employee"}">{text}</a>'
-                 
-                 # link is the column with hyperlinks
-                df1['SIMILAR'] = df1['SIMILAR'].apply(make_clickable)
-                df1 = df1.rename(index={0:'1',1:'2',2:'3',3:'4',4:'5',5:'6',6:'7',7:'8',8:'9',9:'10'})
-                df1 = df1.to_html(escape=False)
-                st.write(df1, unsafe_allow_html=True)
-                    
-        else:
-            if check_spel != str(pos_str.correct()): 
-                
-                temp = calcJaroDistance(check_spel, 1)
-                df_data = pd.DataFrame(temp)
-                df_data = df_data.rename(columns={0: "word"})
-                temp2 = df_data['word']
-                pos_str = temp2.to_string(index=False)
-                
-                # prints the closest similary term
-                st.markdown('<p style="color:red"> Did you mean: {}</p>'.format(str(pos_str)), unsafe_allow_html=True)
-                st.markdown('<p style="color:Blue"> Showing result for : {}</p>'.format(str(pos_str)), unsafe_allow_html=True)
+        dictWordDist = []
+        word_sim = []
+
+        for line in list_model:          
+            wordDistance = jaro_similarity(word, line)
+            word_sim.append(float(wordDistance))
+            dictWordDist.append(line)
+
+        d = {'word':dictWordDist,'word_sim':word_sim}
+
+        # Convert list to pandas
+        df = pd.DataFrame(d)
+        df = df.sort_values(by='word_sim', ascending=False)
+
+        return df.iloc[:numWords]
+
+#get input word
+pos_str = st.text_input('Enter keyword(s)')
+pos_str = pos_str.lower()
+
+# convert the model into vectors
+word_vectors = model.wv
+    
+# Tokenize: Split the sentence into words
+word_list = nltk.word_tokenize(pos_str)
+
+lemmatizer = WordNetLemmatizer()
+# Lemmatize list of words and join
+pos_str = ' '.join([lemmatizer.lemmatize(w) for w in word_list])
+
+#textblob for spelling correction
+check_spel = pos_str
+pos_str = TextBlob(check_spel)  
+ 
+try:
+    if check_spel in word_vectors.vocab:
         
-            pos_str = str(pos_str)
+        pos_str = str(pos_str)
+        # remove spaces both in the beginning and in the end of of string
+        pos_str = re.sub("^\s+|\s+$", "", pos_str, flags=re.UNICODE)
+        
+        # any input that is NOT a-z, A-Z, 0-9,-,*
+        pos_str = re.sub('[^a-zA-Z0-9-_*.]', ' ', pos_str)
+        pos_str = re.sub(' +',' ',  re.sub('\W', ' ', pos_str))
+        
+        pos_words= pos_str.split(' ')
+        
+        
+        if (len(pos_words[0]) > 0):
+        
+            st.write('SIMILAR TO ', pos_str)
+            df = pd.DataFrame(w2v_developed.wv.most_similar(positive = pos_words, topn=10), columns = ['SIMILAR_word', 'similarity'])
+            df1 = df[['SIMILAR_word']]
+            link_list = []
+            for i in df['SIMILAR_word']:
+                          
+                word = 'https://scholar.google.nl/scholar?hl=nl&as_sdt=0%2C5&q=' + i
+         
+                link_list.append(word)
+             # rename column as SIMILAR for UI
             
-             # remove spaces both in the beginning and in the end of of string
-            pos_str = re.sub("^\s+|\s+$", "", pos_str, flags=re.UNICODE)
-            
-            # any input that is NOT a-z, A-Z, 0-9,-,*
-            pos_str = re.sub('[^a-zA-Z0-9-_*.]', ' ', pos_str)
-            pos_str = re.sub(' +',' ',  re.sub('\W', ' ', pos_str))
-            
-            pos_words= pos_str.split(' ')
-            
-            
-            if (len(pos_words[0]) > 0):
-            
-                st.write('SIMILAR TO ', pos_str)
-                df = pd.DataFrame(w2v_developed.wv.most_similar(positive = pos_words, topn=10), columns = ['SIMILAR_word', 'similarity'])
-                df1 = df[['SIMILAR_word']]
-                link_list = []
-                for i in df['SIMILAR_word']:
-                              
-                    word = 'https://scholar.google.nl/scholar?hl=nl&as_sdt=0%2C5&q=' + i
+            df1['SIMILAR'] = link_list
+                          
+            df1 = df1[['SIMILAR']]
+            def make_clickable(link):
+                 # target _blank to open new window
+                 # extract clickable text to display for your link
+                 text = link.split('=')[3]
+                 return f'<a target="_blank" href="{link+" employee"}">{text}</a>'
              
-                    link_list.append(word)
-                 # rename column as SIMILAR for UI
+             # link is the column with hyperlinks
+            df1['SIMILAR'] = df1['SIMILAR'].apply(make_clickable)
+            df1 = df1.rename(index={0:'1',1:'2',2:'3',3:'4',4:'5',5:'6',6:'7',7:'8',8:'9',9:'10'})
+            df1 = df1.to_html(escape=False)
+            st.write(df1, unsafe_allow_html=True)
                 
-                df1['SIMILAR'] = link_list
-                 
-                 #st.dataframe(df1)
-                 
-                df1 = df1[['SIMILAR']]
-                def make_clickable(link):
-                     # target _blank to open new window
-                     # extract clickable text to display for your link
-                     text = link.split('=')[3]
-                     return f'<a target="_blank" href="{link+" employee"}">{text}</a>'
-                 
-                 # link is the column with hyperlinks
-                df1['SIMILAR'] = df1['SIMILAR'].apply(make_clickable)
-                df1 = df1.rename(index={0:'1',1:'2',2:'3',3:'4',4:'5',5:'6',6:'7',7:'8',8:'9',9:'10'})
-                df1 = df1.to_html(escape=False)
-                st.write(df1, unsafe_allow_html=True)
-    except KeyError:
-        st.markdown('<h4 style="color:#bc0031;"> WE ARE SORRY, THE SEARCH TERM IS NOT AVAILABLE IN OUR DATABASE.</h4>', unsafe_allow_html=True) 
+    else:
+        if check_spel != str(pos_str.correct()): 
+            
+            temp = calcJaroDistance(check_spel, 1)
+            df_data = pd.DataFrame(temp)
+            df_data = df_data.rename(columns={0: "word"})
+            temp2 = df_data['word']
+            pos_str = temp2.to_string(index=False)
+            
+            # prints the closest similary term
+            st.markdown('<p style="color:red"> Did you mean: {}</p>'.format(str(pos_str)), unsafe_allow_html=True)
+            st.markdown('<p style="color:Blue"> Showing result for : {}</p>'.format(str(pos_str)), unsafe_allow_html=True)
+    
+        pos_str = str(pos_str)
+        
+         # remove spaces both in the beginning and in the end of of string
+        pos_str = re.sub("^\s+|\s+$", "", pos_str, flags=re.UNICODE)
+        
+        # any input that is NOT a-z, A-Z, 0-9,-,*
+        pos_str = re.sub('[^a-zA-Z0-9-_*.]', ' ', pos_str)
+        pos_str = re.sub(' +',' ',  re.sub('\W', ' ', pos_str))
+        
+        pos_words= pos_str.split(' ')
+        
+        
+        if (len(pos_words[0]) > 0):
+        
+            st.write('SIMILAR TO ', pos_str)
+            df = pd.DataFrame(w2v_developed.wv.most_similar(positive = pos_words, topn=10), columns = ['SIMILAR_word', 'similarity'])
+            df1 = df[['SIMILAR_word']]
+            link_list = []
+            for i in df['SIMILAR_word']:
+                          
+                word = 'https://scholar.google.nl/scholar?hl=nl&as_sdt=0%2C5&q=' + i
+         
+                link_list.append(word)
+             # rename column as SIMILAR for UI
+            
+            df1['SIMILAR'] = link_list
+             
+             #st.dataframe(df1)
+             
+            df1 = df1[['SIMILAR']]
+            def make_clickable(link):
+                 # target _blank to open new window
+                 # extract clickable text to display for your link
+                 text = link.split('=')[3]
+                 return f'<a target="_blank" href="{link+" employee"}">{text}</a>'
+             
+             # link is the column with hyperlinks
+            df1['SIMILAR'] = df1['SIMILAR'].apply(make_clickable)
+            df1 = df1.rename(index={0:'1',1:'2',2:'3',3:'4',4:'5',5:'6',6:'7',7:'8',8:'9',9:'10'})
+            df1 = df1.to_html(escape=False)
+            st.write(df1, unsafe_allow_html=True)
+except KeyError:
+    st.markdown('<h4 style="color:#bc0031;"> WE ARE SORRY, THE SEARCH TERM IS NOT AVAILABLE IN OUR DATABASE.</h4>', unsafe_allow_html=True) 
